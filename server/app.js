7787dfc7b25e9451cfd8c2b35a079a7b79fee2f5
msintaha <msintaha94@gmail.com>
2017-03-26 18:49:54 +0600

    Change server port to 8080

--- server/app.js
+++ server/app.js
@@ -15,6 +15,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('port', process.env.PORT || 8080);
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -40,10 +41,14 @@ app.use(function(req, res, next) {
 });
 
 app.use(function(err, req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(app.get('port'));
 
 module.exports = app;
